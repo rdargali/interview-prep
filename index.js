2c664dc33f95e74d2c3126c1bb25a968b1945009
Rawand Dargali <rdargali@yahoo.com>
2020-03-27 23:22:47 -0500

    questions 3/27

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-// #1 add items to beggining and end of array
+// ****#1 Add items to beggining and end of array
 
-let myArray = ["1", "2", "3", "4", "5"];
+// let myArray = ["1", "2", "3", "4", "5"];
 
-myArray.push("end");
-myArray.unshift("start");
+// myArray.unshift("start");
+// myArray.push("end");
 
-console.log(myArray);
+// myArray = ["start", ...myArray, "end"];
+
+// console.log(myArray);
+
+// ****#2 Create private variable in JS
+
+// function secretVariable() {
+//   let private = "sensitive data";
+
+//   return function() {
+//     return private;
+//   };
+// }
+
+// console.log(secretVariable());
+
+// ****#3 What is the output of console.log(typeof typeof 1)
+
+//type of returns the type of the data in a string form
+// typeof 1 = "number"
+// typeof "number" = string
+// answer is string
+
+// ****#4
